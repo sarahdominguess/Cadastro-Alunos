0bb704011bd258231427354934542698cc15eda1
Sarah Domingues <sarahledomingues@gmail.com>
2025-12-09 19:56:55 -0300

    Inicializando vetores e dicionário para armazenar dados dos alunos e inserindo função para coletar esses dados
    Added data structures for student registration.

--- cadastro_alunos.py
+++ cadastro_alunos.py
@@ -1,9 +1,35 @@
-# Importando bibliotecas pandas e os
+# Importando bibliotecas pandas e funcoes
 import pandas as pd
 import funcoes as fc
 
 # Carregando arquivo csv com função
 df = fc.carrega_csv("cadastro_alunos.csv")
 
+nomes = []
+rua = []
+numero = []
+bairro = []
+cidade = []
+uf = []
+telefone = []
+email = []
+matricula = []
+
+cadastros = {
+    'nome': nomes,
+    'rua': rua,
+    'numero': numero,
+    'bairro': bairro,
+    'cidade': cidade,
+    'uf': uf,
+    'telefone': telefone,
+    'email': email,
+    'matricula': matricula
+}
+
 # Recolhendo ação que usuario deseja realizar
 opcao = fc.menu()
+
+# Se o usuário escolher inserir um aluno
+if opcao == 1:
+    fc.inserir(nomes, rua, numero, bairro, cidade, uf, telefone, email)
