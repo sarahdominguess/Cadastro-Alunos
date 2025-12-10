9433dc5e28298c2e93ec78165d9afdb8e5b8c306
Sarah Domingues <sarahledomingues@gmail.com>
2025-12-10 16:02:15 -0300

    Comentando código

--- cadastro_alunos.py
+++ cadastro_alunos.py
@@ -2,34 +2,33 @@
 import pandas as pd
 import funcoes as fc
 
-# Recolhendo ação que usuario deseja realizar
+# Iniciando variável
 opcao = 0
 
-while opcao != 3:
+while opcao != 3: # Enquanto opcao for diferente de 3 (diferente de SAIR)
     opcao = fc.menu()
 
     # Carregando arquivo csv com função
     df = fc.carrega_csv("cadastro_alunos.csv")
 
-    # Se o usuário escolher inserir um aluno
-    if opcao == 1:
-        df = fc.inserir(df)
-        fc.salvar(df, "cadastro_alunos.csv")
-        opcao = 0
+    if opcao == 1: # Se o usuário escolher inserir um aluno
+        df = fc.inserir(df) # Inserindo aluno com função
+        fc.salvar(df, "cadastro_alunos.csv") # Salvando o dataframe em csv com função
+        opcao = 0 # 'Resetando' opcao
 
-    elif opcao == 2:
-        pesquisa = fc.pesquisar(df)
+    elif opcao == 2: # Se o usuário escolher pesquisar um aluno
+        pesquisa = fc.pesquisar(df) # Pesquisando aluno com função
         
-        if pesquisa.empty == False:
-            opcao_pesquisa = fc.pesquisa_menu()
+        if pesquisa.empty == False: # Se o aluno for encontrado no sistema
+            opcao_pesquisa = fc.pesquisa_menu() # Pesquisando aluno com Função
 
-            if opcao_pesquisa == 1:
-                df = fc.editar(df, pesquisa)
-                fc.salvar(df, "cadastro_alunos.csv")
-            elif opcao_pesquisa == 2:
-                df = fc.remover(df, pesquisa)
-                fc.salvar(df, "cadastro_alunos.csv")
+            if opcao_pesquisa == 1: # Se o usuário escolher editar alguma informação
+                df = fc.editar(df, pesquisa) # Editando informação com função
+                fc.salvar(df, "cadastro_alunos.csv") # Salvando o dataframe em csv com função
+            elif opcao_pesquisa == 2: # Se o usuário escolher remover algum aluno
+                df = fc.remover(df, pesquisa) # Removendo aluno com função
+                fc.salvar(df, "cadastro_alunos.csv") # Salvando o dataframe em csv com função
         
-        opcao = 0
+        opcao = 0 # 'Resetando' opcao
 
-print("\nPrograma finalizado!")
+print("\nPrograma finalizado!") # Mensagem de finalização
