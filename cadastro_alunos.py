44cd6cccf13549f657adc32f2c0de0d27a6f0549
Sarah Domingues <sarahledomingues@gmail.com>
2025-12-10 15:11:35 -0300

    Implementando loop e apenas realizando funções de edição ou remoção de aluno caso ele tenha sido encontrado no sistema

--- cadastro_alunos.py
+++ cadastro_alunos.py
@@ -2,25 +2,34 @@
 import pandas as pd
 import funcoes as fc
 
-# Carregando arquivo csv com função
-df = fc.carrega_csv("cadastro_alunos.csv")
-
 # Recolhendo ação que usuario deseja realizar
-opcao = fc.menu()
+opcao = 0
 
-# Se o usuário escolher inserir um aluno
-if opcao == 1:
-    df = fc.inserir(df)
+while opcao != 3:
+    opcao = fc.menu()
 
-elif opcao == 2:
-    pesquisa = fc.pesquisar(df)
-    opcao_pesquisa = fc.pesquisa_menu()
+    # Carregando arquivo csv com função
+    df = fc.carrega_csv("cadastro_alunos.csv")
 
-    if opcao_pesquisa == 1:
-        df = fc.editar(df, pesquisa)
-    elif opcao_pesquisa == 2:
-        df = fc.remover(df, pesquisa)
-        print(df)
+    # Se o usuário escolher inserir um aluno
+    if opcao == 1:
+        df = fc.inserir(df)
+        fc.salvar(df, "cadastro_alunos.csv")
+        opcao = 0
 
-elif opcao == 3:
-    print("\nPrograma finalizado!")
+    elif opcao == 2:
+        pesquisa = fc.pesquisar(df)
+        
+        if pesquisa.empty == False:
+            opcao_pesquisa = fc.pesquisa_menu()
+
+            if opcao_pesquisa == 1:
+                df = fc.editar(df, pesquisa)
+                fc.salvar(df, "cadastro_alunos.csv")
+            elif opcao_pesquisa == 2:
+                df = fc.remover(df, pesquisa)
+                fc.salvar(df, "cadastro_alunos.csv")
+        
+        opcao = 0
+
+print("\nPrograma finalizado!")
